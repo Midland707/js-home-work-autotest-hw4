15c827d3e6362681ef47f288d7722c155f252ede
Midland707 <rus14job@ukr.net>
2022-11-10 17:54:48 +0200

    add autotest 1-4

--- mod4-auto2.js
+++ mod4-auto2.js
@@ -1,1 +1,18 @@
 //MODUL4 AUTOTEST2
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  console.log(callback(pizzaName));
+  return callback(pizzaName);
+}
+
+makeMessage("Royal Grand", makePizza); //повертає рядок "Pizza Royal Grand is being prepared, please wait..."
+makeMessage("Ultracheese", deliverPizza); //повертає рядок "Delivering Ultracheese pizza."
--- mod4-auto4.js
+++ mod4-auto4.js
@@ -1,2 +1,31 @@
 //MODUL4 AUTOTEST4
 
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.find((pizza) => pizzaName === pizza)) {
+      return onSuccess(pizzaName);
+    } else return onError(pizzaName);
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  console.log(`Your order is accepted. Cooking pizza ${pizzaName}.`);
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  console.log(
+    `Error! There is no pizza with a name ${error} in the assortment.`
+  );
+  return `Error! There is no pizza with a name ${error} in the assortment.`;
+}
+
+// Method calls with callbacks
+pizzaPalace.order("Smoked", makePizza, onOrderError); //повертає  "Your order is accepted. Cooking pizza Smoked."
+pizzaPalace.order("Four meats", makePizza, onOrderError); //повертає  "Your order is accepted. Cooking pizza Four meats."
+pizzaPalace.order("Big Mike", makePizza, onOrderError); //повертає  "Error! There is no pizza with a name Big Mike in the assortment."
+pizzaPalace.order("Vienna", makePizza, onOrderError); //повертає  "Error! There is no pizza with a name Vienna in the assortment."
