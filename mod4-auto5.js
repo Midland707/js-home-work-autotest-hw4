fc3bb352b52555420d0d0f08548f42860937cc36
Midland707 <rus14job@ukr.net>
2022-11-10 18:09:01 +0200

    add autotest 5

--- mod4-auto5.js
+++ mod4-auto5.js
@@ -1,2 +1,17 @@
 //MODUL4 AUTOTEST5
 
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+
+  orderedItems.forEach(function (item) {
+    totalPrice += item;
+  });
+  console.log(totalPrice);
+  // Change code above this line
+  return totalPrice;
+}
+
+calculateTotalPrice([12, 85, 37, 4]); //повертає 138
+calculateTotalPrice([164, 48, 291]); //повертає 503
+calculateTotalPrice([412, 371, 94, 63, 176]); //повертає 1116
