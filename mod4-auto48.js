093551d7185a0f89449f1d341a7cf03bd2ebbeab
Midland707 <rus14job@ukr.net>
2022-11-12 00:48:41 +0200

    add autotest 48

--- mod4-auto48.js
+++ mod4-auto48.js
@@ -1,1 +1,86 @@
 //MODUL4 AUTOTEST48
+
+const loadUsers = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+// Change code below this line
+const getTotalBalanceByGender = (users, gender) => users
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => {
+        return totalBalance + user.balance;
+    }, 0);
+// Change code above this line
+getTotalBalanceByGender(loadUsers, "male");
+getTotalBalanceByGender(loadUsers, "female");
+console.log("getTotalBalanceByGender - ", getTotalBalanceByGender(loadUsers, "male"));
+console.log("getTotalBalanceByGender - ", getTotalBalanceByGender(loadUsers, "female"));
+
+// Доповни функцію getTotalBalanceByGender(users, gender) таким чином,
+//     щоб вона повертала загальний баланс користувачів(властивість balance),
+//         стать яких(властивість gender) збігається зі значенням параметра gender.
+
+// Якщо значення параметра gender - це рядок "male", функція повертає число 12053
+// Якщо значення параметра gender - це рядок "female", функція повертає число 8863
