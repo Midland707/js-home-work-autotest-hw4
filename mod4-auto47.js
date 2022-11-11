321cec02423eef1f777da76cdc3131ee5809b135
Midland707 <rus14job@ukr.net>
2022-11-11 23:44:24 +0200

    add autotest 47

--- mod4-auto47.js
+++ mod4-auto47.js
@@ -1,1 +1,86 @@
 //MODUL4 AUTOTEST47
+
+const loadUsers = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+// Change code below this line
+const getSortedFriends = users => [...users]
+    .flatMap(user => user.friends)
+    .filter((friend, index, array) => array.indexOf(friend) === index)
+    .sort();
+// Change code above this line
+
+
+getSortedFriends(loadUsers);
+console.log("getSortedFriends - ", getSortedFriends(loadUsers));
+
+// Доповни функцію getSortedFriends(users) таким чином, щоб вона повертала
+// масив унікальних імен друзів(властивість friends), відсортований за алфавітом.
+
+// Виклик функції із зазначеним масивом користувачів повертає масив
+// ["Adrian Cross", "Aisha Tran", "Briana Decker", "Eddie Strong", "Goldie Gentry",
+//     "Jacklyn Lucas", "Jordan Sampson", "Linda Chapman", "Marilyn Mcintosh", "Naomi Buckner",
+//     "Padilla Garrison", "Sharron Pace", "Solomon Fokes"]
